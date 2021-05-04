279da41db6ea2c10d87ff01c086049be7b72e106
Adjair Costa Farias <adjaircfarias@gmail.com>
2021-05-03 21:29:11 -0300

    novas validacoes

---
+++ AcfLib/Cep/ValidacaoCep.cs
@@ -0,0 +1,16 @@
+﻿namespace AcfLib.Cep
+{
+    internal abstract class ValidacaoCep
+    {
+        public bool ValidarCep(string cep)
+        {
+            if (string.IsNullOrWhiteSpace(cep))
+                return false;
+
+            cep = cep.Trim();
+            cep = cep.Replace("-", "");
+
+            return cep.Length == 8;
+        }
+    }
+}
