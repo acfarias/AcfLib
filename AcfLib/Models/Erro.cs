f24d0ed6d1dc83822035d8ac78dd293d3fca82df
Adjair Costa Farias <adjaircfarias@gmail.com>
2021-05-04 19:55:18 -0300

    melhorias filter exception

---
+++ AcfLib/Models/Erro.cs
@@ -0,0 +1,14 @@
+﻿namespace AcfLib.Models
+{
+    internal sealed class Erro
+    {
+        public Erro(string codigo, string descricao)
+        {
+            Codigo = codigo;
+            Descricao = descricao;
+        }
+
+        public string Codigo { get; private set; }
+        public string Descricao { get; private set; }
+    }
+}
